f80ebc91e157630d8a1110f1bdfcab5f381befe0
japaSKCR <japa_silva696@hotmail.com>
2018-11-09 02:20:31 -0300

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var moveUp = new Vector3(0,0,1);
+        var moveDown = new Vector3(0,0,-1);
+        var moveLeft = new Vector3(-1,0,0);
+        var moveRight = new Vector3(1,0,0);
+
+        float velocity = 0.2f;
+
+        var displacement = new Vector3(0,0,0);
+
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            displacement += moveUp ;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+           displacement += moveLeft;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            displacement += moveDown;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+           displacement += moveRight;
+        }
+
+        displacement.Normalize();
+
+        transform.position += displacement * velocity;
+
+    }
+}
---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public float speedH = 2.0f;
+    public  float speedV = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        var speed = 0.05f;
+        var yAxis = new Vector3(0, 1, 0);
+        var xAxis = new Vector3(1, 0, 0);
+
+        var k = new Vector3(
+            transform.worldToLocalMatrix.m20,
+           0,
+            transform.worldToLocalMatrix.m22);
+        var i = new Vector3(
+            transform.worldToLocalMatrix.m00,
+            transform.worldToLocalMatrix.m01,
+            transform.worldToLocalMatrix.m02);
+
+        k.Normalize();
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -k * speed;
+            //descomentar para ulilizar o efeito dolly zoom 
+            //GetComponent<Camera>().fieldOfView--;  
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += k * speed;
+            
+            //descomentar para ulilizar o efeito dolly zoom 
+           //GetComponent<Camera>().fieldOfView++;
+        }
+
+        if (Input.GetKey(KeyCode.A)) transform.position += -i * speed ;
+        if (Input.GetKey(KeyCode.D)) transform.position += i * speed ;
+
+
+
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(yAxis, -1, Space.Self);
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Rotate(yAxis, 1, Space.Self);
+        if (Input.GetKey(KeyCode.UpArrow)) transform.Rotate(xAxis, -1, Space.Self);
+        if (Input.GetKey(KeyCode.DownArrow)) transform.Rotate(xAxis, 1, Space.Self);
+
+
+
+        //yaw += speedH * Input.GetAxis("Mouse X");
+        //pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        //transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+
+
+    }
+}
